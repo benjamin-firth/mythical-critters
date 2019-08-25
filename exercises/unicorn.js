9b9c3d91d72e91ec6f36f01f7dc0c4bfa5ae111f
Ben Firth <bfirth93@gmail.com>
2019-08-25 10:13:35 -0600

    Unicorn walkthrough work

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,5 +1,19 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color) {
+    this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
+  }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+  says(message) {
+    return (`**;* ${message} *;**`);
   }
 }
 
