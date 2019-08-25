61e852316a56162ed2f558273e9653f63b1548d1
Ben Firth <bfirth93@gmail.com>
2019-08-25 13:58:06 -0600

    Add vampire solo challenge

---
+++ exercises/vampire.js
@@ -0,0 +1,21 @@
+class Vampire {
+  constructor (name, pet, thirsty) {
+    this.name = name;
+    if (pet === undefined) {
+      this.pet = 'bat';
+    } else {
+      this.pet = pet;
+    }
+    if (thirsty === undefined) {
+      this.thirsty = true;
+    }
+  }
+
+  drink()  {
+    this.thirsty = false; 
+  }
+}
+
+
+
+module.exports = Vampire;
