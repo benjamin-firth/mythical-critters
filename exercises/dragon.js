b805c592450050c3ca466ea44c950c09be733b09
Ben Firth <bfirth93@gmail.com>
2019-08-26 22:53:40 -0600

    Dragon test

---
+++ exercises/dragon.js
@@ -0,0 +1,23 @@
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.hungry = true;
+    this.hungryCounter = 0;
+  }
+
+  eat() {
+    if (this.hungryCounter < 2) {
+      this.hungry = true;
+      this.hungryCounter += 1;
+    } else {
+      console.log('firing')
+      this.hungry = false;
+    }
+    console.log(this.hungryCounter);
+  }
+};
+
+
+module.exports = Dragon;
